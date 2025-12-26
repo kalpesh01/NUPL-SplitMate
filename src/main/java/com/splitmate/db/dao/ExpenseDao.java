400b40da9651715af158eb25b71dd18bf997c52e
kalpesh <pkalpesh1996@gmail.com>
2025-12-26 14:59:51 +0530

    refactor: dto to DTO name changed conflicts resolve

--- src/main/java/com/splitmate/db/dao/ExpenseDao.java
+++
@@ -1,8 +0,0 @@
-package com.splitmate.db.dao;
-
-import com.splitmate.db.entity.ExpenseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseDAO extends JpaRepository<ExpenseEntity, Long> {
-}
-
